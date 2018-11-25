a834360f23a6c7e2a312da143a50be7a487ce00b
Sky <hugheschiu@gmail.com>
2018-11-25 23:34:38 +0800

    update route selector

--- src/pages/route/route.ts
+++ src/pages/route/route.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, Events, NavParams, NavController } from 'ionic-angular';
 import { DrawerState } from 'ion-bottom-drawer';
-import { GlobalParamsProvider } from '../../providers/global-params/global-params';
-import { CameraPage } from '../camera/camera';
-import { MyApp } from '../../app/app.component';
-
 
 /**
  * Generated class for the RoutePage page.
@@ -30,8 +26,7 @@ export class RoutePage {
   constructor(
     public navParams: NavParams,
     public events: Events,
-    private globalParamsProvider: GlobalParamsProvider,
-    public navCtrl: NavController
+    private navCtrl: NavController
     ) 
     {
 
@@ -42,8 +37,8 @@ export class RoutePage {
   }
 
   navigateToOtherPage(): void {
-    // this.navCtrl.push(CameraPage);
-    this.globalParamsProvider.nav.push(CameraPage);
+    this.nowState = DrawerState.Bottom;
+    this.navCtrl.parent.select(3);
   }
 
   ionViewDidLoad() {
@@ -51,24 +46,17 @@ export class RoutePage {
   }
 
   ionViewDidLeave(){
-    this.nowState = DrawerState.Bottom;
-  }
-
-  viewWillEnter() {
-    console.log('viewwillenter');
-    this.nowState = DrawerState.Docked;
   }
 
   routeGenerator() {
-    // this.navCtrl.push(MyApp);
-    this.globalParamsProvider.nav.push(MyApp);
+    this.nowState = DrawerState.Bottom;
+    this.navCtrl.parent.select(0);
   }
 
   stateChange(state){
     console.log(this.nowState);
     if(this.nowState === 0){
-      this.globalParamsProvider.nav.push(MyApp);
-      this.navCtrl.push(MyApp);
+      this.navCtrl.parent.select(0);
     }
   }
 }
